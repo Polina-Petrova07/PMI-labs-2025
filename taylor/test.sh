58b7e5608ad7f56036c535ab2d594387bd66c82e
Владислав Тончаров <Kotauskas@yandex.ru>
2025-03-20 19:50:21 +0300

    Make `test.sh` cleaner

--- taylor/test.sh
+++ taylor/test.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
+set -uo pipefail
+
+outdir='/tmp/taylor_result'
+
 function run {
-    ./taylor "$1" "$2" "$3" "$4" "$5" | tr '\036\037' '\n,'
+    ./taylor 2>/dev/null > "$outdir/$4$5.csv" # Print header
+    ./taylor "$1" "$2" "$3" "$4" "$5" | tr '\036\037' '\n,' >> "$outdir/$4$5.csv"
 }
 
-trig_start=-6.28
-trig_end=6.28
+trig_start=-2
+trig_end=2
 trig_step=0.1
 
 exp_start=-2
 exp_end=2.5
-exp_step=0.15
+exp_step=0.1
 
-ln1p_start=-0.9
-ln1p_end=1
-ln1p_step=0.1
+ln1p_start=-0.95
+ln1p_end=1.05
+ln1p_step=0.05
 
-./taylor 2>/dev/null # Print header
-for terms in 4 32; do
+mkdir "$outdir" || exit
+
+for terms in 4 10; do
     run $trig_start $trig_end $trig_step sin  $terms
     run $trig_start $trig_end $trig_step cos  $terms
     run $exp_start  $exp_end  $exp_step  exp  $terms
+done
+
+for terms in 4 32; do
     run $ln1p_start $ln1p_end $ln1p_step ln1p $terms
 done
